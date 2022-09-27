0abbbcffa689c28fa129f83984fabb0dc2354a28
Mena Aziz <menaaziz27@gmail.com>
2022-09-27 12:02:20 +0200

    global styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,19 @@ import PrivateRoute from './hocs/PrivateRoute';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import GamesPage from './pages/GamesPage';
 import TeamsPage from './pages/TeamsPage';
-import './app.css';
+import './App.css';
+import TeamDetails from './pages/TeamDetails';
 
 function App() {
 	return (
 		<Router>
 			<Navbar />
 			<Route exact path="/" component={Home} />
-			<Route path="/login" component={Login} />
-			<Route path="/register" component={Register} />
-			<PrivateRoute path="/games" component={GamesPage} />
-			<PrivateRoute path="/teams" component={TeamsPage} />
+			<Route exact path="/login" component={Login} />
+			<Route exact path="/register" component={Register} />
+			{/* <PrivateRoute path="/games" component={GamesPage} /> */}
+			<PrivateRoute exact path="/teams" component={TeamsPage} />
+			<PrivateRoute exact path="/teams/:id" component={TeamDetails} />
 		</Router>
 	);
 }
