79e34731f39b45c1f5f3a1972100e61b2a29d6b5
Mena Aziz <menaaziz27@gmail.com>
2022-10-04 12:54:32 +0200

    fixed typo

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require('./services/jwtStrategy');
 app.use('/api', require('./routes'));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/client//build')));
+	app.use(express.static(path.join(__dirname, '/client/build')));
 
 	app.use('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
